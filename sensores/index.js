5fdd7e0fdd6f8897acaa49185653beba5a1df664
Filipe Silva <filipe.ribeirosilva22@gmail.com>
2025-06-17 23:12:52 -0300

    final

--- sensores/index.js
+++ sensores/index.js
@@ -14,7 +14,7 @@ const node_name = process.env.NODE_NAME;
 const min = parseFloat(process.env.MIN_VALUE) || 0;
 const max = parseFloat(process.env.MAX_VALUE) || 100;
 const variacao = parseFloat(process.env.VARIATION) || 5;
-
+const unidade = process.env.UNIDADE || 'unidade';
 
 const sensor = new SensorSimulado(node_name, min, max, variacao);
 
@@ -24,7 +24,12 @@ await mqttConn.connect({ ...getTlsFiles(node_name) });
 
 setInterval(() => {
     const data = sensor.lerValor();
-    const payload = JSON.stringify({   from: node_name, time: new Date().toISOString(), value: data });
+    const payload = JSON.stringify(
+        {   sensor: node_name, 
+            timestamp: new Date().toISOString(), 
+            valor: data,
+            unidade
+        });
 
     console.log(`📊 Enviando dados do sensor ${node_name}: ${data}`);
 
